cf89468dad642c628f02680a16dabc8e706671e5
unknown <moinahmed100@gmail.com>
2021-11-10 11:22:22 -0500

    Added class example with constructor

--- class_example.cpp
+++ class_example.cpp
@@ -6,20 +6,48 @@
 using namespace std;
 
 class Book {
+/*
+ Creating a Book class
+ -----------------------
+ ------------------------
+ Atrributes
+ -------------------------
+ 1. Title
+ 2. Author
+ 3. Pages
+ */
 public:
     string title;
     string author;
     int pages;
+
+    // First constructor function
+    Book() {
+        title = "no title";
+        author = "no author";
+        pages = 0;
+    }
+
+    //Second constructor function
+    Book(string aTitle, string aAuthor, int aPages)
+    {
+        title = aTitle;
+        author = aAuthor;
+        pages = aPages;
+    }
 };
 
 int main()
 {
     Book book1;
-    book1.title = "Electrochemical Systems";
-    book1.author = "Newmann";
-    book1.pages = 500;
 
     cout << book1.title << endl;
     cout << book1.author << endl;
     cout << book1.pages << endl;
+
+    Book book2("Harry Potter", "JK Rowling", 500);
+
+    cout << book2.title << endl;
+    cout << book2.author << endl;
+    cout << book2.pages << endl;
 }
